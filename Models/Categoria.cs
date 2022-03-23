ccf52108f6a3679ed2f95f223ecaaa19bb22d657
Marcelo Angelo Gomes <marcelo.gomes01@educacao.sp.gov.br>
2022-03-23 08:08:11 -0300

    Inicial

---
+++ Models/Categoria.cs
@@ -0,0 +1,9 @@
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id {get;set;}
+        public string Descricao{get;set;}
+        
+    }
+}
---
+++ Models/Context.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias {get;set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CursoMVC;Integrated Security=True");
+        }
+
+    }
+}
